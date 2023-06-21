30a0752c132e53da85232a9407e3216a724ed3f2
Shubham <singhshubham7906@gmail.com>
2023-06-21 23:59:11 +0530

    First Commit

---
+++ script.js
@@ -0,0 +1,46 @@
+const accessKey = "K98G0bp13l-A-kA-_pUmsrq8MjJQYsHUs4aJGA3Aa8c";
+
+const searchForm = document.querySelector('#search-form')
+const searchBox = document.querySelector('#search-box')
+const searchResult = document.querySelector('#search-result')
+const btn = document.querySelector('#btn')
+
+let keyword = "";
+let page = 1;
+
+async function searchImages(){
+    keyword = searchBox.value;
+    const url = `https://api.unsplash.com/search/photos?page=${page}&query=${keyword}&client_id=${accessKey}&per_page=12`;
+
+    const response = await fetch(url);
+    const data = await response.json();
+
+    if(page === 1){
+        searchResult.innerHTML = "";
+    }
+
+    const results = data.results;
+
+    results.map((result)=>{
+        const image = document.createElement('img');
+        image.src = result.urls.small;
+        const imageLink = document.createElement("a");
+        imageLink.href = result.links.html;
+        imageLink.target = "_blank";
+
+        imageLink.appendChild(image);
+        searchResult.appendChild(imageLink);
+    })
+    btn.style.display = "block";
+}
+
+searchForm.addEventListener("submit", (e)=>{
+    e.preventDefault();
+    page = 1;
+    searchImages();
+});
+
+btn.addEventListener('click',function(e){
+    page++;
+    searchImages();
+})
